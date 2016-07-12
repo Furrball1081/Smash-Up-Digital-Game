dcc51c358f20d77bbc2cbb6a766813e39f560511
Furrball1081 <CFurry66@gmail.com>
2016-07-11 20:34:33 -0500

    Full Project for use
    Got rid of zip folder and added regular folder for better use in version
    control later on

---
+++ Digital Smash up/Assets/Scripts/FactionScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class FactionScript : MonoBehaviour {
+	public float deckNum = 20f;
+	public List<GameObject> Cards;
+	public string factionName;
+
+	
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+
+
+	public void AddToDeck(){
+	
+		foreach (Transform child in transform) {
+			if (child.tag == ("Card")) {
+				Cards.Add(child.gameObject);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
